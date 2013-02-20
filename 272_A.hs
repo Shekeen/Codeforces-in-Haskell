ebf8bde0f4cc234d08b8618f170b70b09213a8a3
Anton Guryanov <gurrrik@yandex-team.ru>
2013-02-20 11:11:52 +0400

    272A problem solution

---
+++ 272_A.hs
@@ -0,0 +1,8 @@
+module Main where
+
+solve :: [Int] -> Int
+solve xs = let l = length xs
+               s = sum xs
+           in foldl (\ acc x -> if x == 1 then acc else acc+1) 0 $ map ((`mod` (l+1)).(+s)) [1,2,3,4,5]
+
+main = getLine >> getLine >>= (print . solve . (map read) . words)
